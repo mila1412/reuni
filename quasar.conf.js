07e197012cd4a1a1d0b0cf8183ae6414a22bb1fa
mila1412 <k120240305@gmail.com>
2021-08-17 17:47:23 +0800

    .env

--- quasar.conf.js
+++ quasar.conf.js
@@ -50,7 +50,8 @@ module.exports = function (/* ctx */) {
     // Full list of options: https://v1.quasar.dev/quasar-cli/quasar-conf-js#Property%3A-build
     build: {
       vueRouterMode: 'hash', // available values: 'hash', 'history'
-      build: {
+      env: {
+        API_URL: process.env.API_URL
       },
       // transpile: false,
 
